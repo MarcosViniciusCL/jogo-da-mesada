fa007220dccad139c174908b9589d27a0442f3ef
marcos <marcos@DESKTOP-D4GNQSV>
2017-05-08 10:56:19 -0300

    Adiciona: Comunicação basica com controller de conexão, reenviar mensagem perdida(talvez seja alterado para outro controller) e jogar o dado caso disponivel.

--- src/pbl/controller/ControllerJogo.java
+++ src/pbl/controller/ControllerJogo.java
@@ -5,10 +5,67 @@
  */
 package pbl.controller;
 
+import pbl.exception.AguadarVezException;
+import java.io.IOException;
+import pbl.model.Dado;
+
 /**
  *
  * @author marcos
  */
 public class ControllerJogo {
+
+    private static ControllerJogo controllerJogo; //Instancia da propria classe.
+    private final ControllerConexao controllerConexao; //Instancia do controller de conexão.
+
+    //ATRIBUTOS DO JOGO
+    private boolean minhaVez; //Variavel para informar se é a vez do cliente jogar o dado;
+    private final Dado dado; //Dado do jogo
+    private String ultimaMens; //Fica salva a ultima mensagem que foi enviada ao grupo para reenvio caso tenha perda.
+
+    private ControllerJogo() {
+        this.controllerConexao = ControllerConexao.getInstance();
+        this.dado = new Dado();
+    }
+
+    public static ControllerJogo getInstance() {
+        if (controllerJogo == null) {
+            controllerJogo = new ControllerJogo();
+        }
+        return controllerJogo;
+    }
+    //****************************************** METODOS RESPONSAVEIS PELA AÇÃO DO JOGO ************************************
+
+    /**
+     * Joga o dado e retorna o valor que o dado permaneceu.
+     *
+     * @return valor - Valor final do dado
+     * @throws AguadarVezException - Ainda não é a vez do jogador.
+     */
+    public int jogarDado() throws AguadarVezException {
+        int valor;
+        if (minhaVez) {
+            valor = dado.jogarDado();
+        } else {
+            throw new AguadarVezException();
+        }
+        return valor;
+    }
+
     
+    
+    
+    
+    
+    //****************************************** METODOS RESPONSAVEIS PELA COMUNICAÇÃO ************************************
+    
+    /**
+     * Reenvia a ultima mensagem que foi enviada ao grupo multcast.
+     *
+     * @throws java.io.IOException
+     */
+    public void reenviarUltMensGRP() throws IOException {
+        controllerConexao.enviarMensagemGRP(ultimaMens);
+    }
+
 }
