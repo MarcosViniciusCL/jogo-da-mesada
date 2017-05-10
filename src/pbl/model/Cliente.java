51a60ef570534aa18a4be8a8729267488dd08402
marcos <marcos@DESKTOP-D4GNQSV>
2017-05-10 18:02:38 -0300

    Adicionado: Gerenciamento do controle do jogo

---
+++ src/pbl/model/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbl.model;
+
+/**
+ *
+ * @author marcos
+ */
+public class Cliente {
+    private int identificacao;
+    private String ultimaMensagem;
+    private int contadorUltMens;
+}
