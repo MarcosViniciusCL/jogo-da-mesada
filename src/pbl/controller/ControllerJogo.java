4cc431ddef786a898ae163aa3a171d77d1a7cd91
emerson <emerson@unix>
2017-05-26 13:26:08 -0300

    Adição do metodo achou um realizar emprestimo

--- src/pbl/controller/ControllerJogo.java
+++ src/pbl/controller/ControllerJogo.java
@@ -91,6 +91,15 @@ public class ControllerJogo {
         }
         return valor;
     }
+    
+    public void realizarEmprestimo(Jogador j, double valor){
+        if(valor>0)
+            j.getConta().realizarEmprestimo(valor);
+    }
+    
+    /**
+     * Cria uma nova instancia da lista bolão de esportes
+     */
 
     public void novoBolaoDeEsportes() {
         bilhetesBolao = new ArrayList<>();
